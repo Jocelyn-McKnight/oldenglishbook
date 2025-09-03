333bd7c20d1234aa11fb686985884d903269e4ac
Jocelyn McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2025-09-02 21:22:37 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://jocelyn-mcknight.github.io/oldenglishbook/home",
+    baseUrl: "https://jocelyn-mcknight.github.io/oldenglishbook/Syllabus",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
